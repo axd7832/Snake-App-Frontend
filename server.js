72089c10a3620f144dad6b40aa2ba3d524f36c85
Andrew Diana <axd7832@g.rit.edu>
2018-10-25 01:27:59 -0400

    um

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express')
+// const serveStatic = require('serve-static')
+const path = require('path')
 const history = require('connect-history-api-fallback')
+// Create default port to serve the app on
+const port = process.env.PORT || 5000
+
+// create the express app
 const app = express()
+// create middleware to handle serving the app
+app.use(history())
 
-const staticFileMiddleware = express.static(__dirname)
-app.use(staticFileMiddleware)
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-}))
-app.use(staticFileMiddleware)
-
-const port = process.env.PORT || 5555
-app.listen(port, () => {
-  console.log(`Example app listening on port ${5555}!`)
+app.use(express.static(`${__dirname}/dist/`))
+// Catch all routes and redirect to the index file (Using vue-router history)
+app.get('/.*/', function (req, res) {
+  res.sendfile(path.join(`${__dirname}/dist/index.html`))
 })
+app.listen(port)
+// Log to feedback that this is actually running
+console.log('Server started on port ' + port)
