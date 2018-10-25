6107279c7048e32aff52b1688252e198f7876035
Andrew Diana <axd7832@g.rit.edu>
2018-10-25 00:25:46 -0400

    still...trying

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var history = require('connect-history-api-fallback')
 
 // create the express app
 const app = express()
-// create middleware to handle the serving the app
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// create middleware to handle serving the app
 app.use(history())
-// Catch all routes and redirect to the index file
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, '/dist/index.html'))
-// })
+app.use('/', serveStatic(`${__dirname}'/dist/index.html`))
+// Catch all routes and redirect to the index file (Using vue-router history)
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 // Create default port to serve the app on
 const port = process.env.PORT || 5000
 app.listen(port)
